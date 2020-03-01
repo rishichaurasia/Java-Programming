5d16c24340f96700397280614261bd3f77d10290
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-03-02 04:30:21 +0530

    Have to solve it more efficiently

--- Learning Java/src/Assignment/DictionaryOrderLarger.java
+++ Learning Java/src/Assignment/DictionaryOrderLarger.java
@@ -10,8 +10,7 @@ public class DictionaryOrderLarger {
 		Scanner scn = new Scanner(System.in);
 		String str = scn.next();
 		String str_sorted = sortString(str);
-		System.out.println(str_sorted);
-//		dictionaryOrderLarger(str);
+		dictionaryOrderLarger(str, str_sorted, "");
 
 	}
 	
@@ -25,12 +24,28 @@ public class DictionaryOrderLarger {
 	}
 	
 	public static void dictionaryOrderLarger(String str, String str_sort, String ans){
-		
+		if(str_sort.length()==0) {
+			if(isLarger(str, ans))
+				System.out.println(ans);
+			return;
+		}
 		
 		for(int i = 0; i<str_sort.length(); i++) {
 			char ch = str_sort.charAt(i);
-//			if(ch>=str.charAt(0))
+			String roq = str_sort.substring(0, i) + str_sort.substring(i+1);
+			dictionaryOrderLarger(str, roq, ans+ch);
 		}
 	}
+	
+	public static boolean isLarger(String str, String ans) {
+		for(int i = 0; i<str.length(); i++) {
+			if(ans.charAt(i) < str.charAt(i))
+				return false;
+			if(ans.charAt(i) > str.charAt(i))
+				return true;
+		}
+		return false;
+	}
+	
 
 }
