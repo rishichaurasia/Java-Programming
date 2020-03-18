b1548892c661c359cb54123f6fbe4fd1721fdd07
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-03-19 02:51:42 +0530

    To be solved

---
+++ Learning Java/src/LeetCodeProblems/BinaryWatch.java
@@ -0,0 +1,48 @@
+package LeetCodeProblems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BinaryWatch {
+
+//	static Scanner scn = new Scanner(System.in);
+//	
+//	public static void main(String[] args) {
+//		
+//		int n = scn.nextInt();
+//		List<String> res = getTime(n);
+//		System.out.println(res);
+//		
+//	}
+//	
+//	public static List<String> getTime(int n){
+//		
+//		List<String> res = new ArrayList<String>();
+//		List<String> hrs = new ArrayList<String>();
+//		List<String> min = new ArrayList<String>();
+//		for(int i = 0; i<=n; i++) {
+//			hrs.addAll(getHrs(i));
+//		}
+//		return res;
+//		
+//	}
+//
+//	public static List<Integer> getHrs(int n, int i){
+//		
+//		if(n == 0 ) {
+//			List<Integer> br = new ArrayList<Integer>();
+//			br.add(0);
+//			return br;
+//		}
+//		List<Integer> rr = getHrs(n-1, i*2);
+//		List<Integer> mr = new ArrayList<Integer>();
+//		for(int val : rr) {
+//			mr.add(val);
+//			mr.add(val+i);
+//		}
+//		
+//		
+//	}
+	
+}
