a7913797761b522356041477e2de9f46188afe92
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-03-25 21:25:44 +0530

    Next Greater Element in order
    Important

--- Crux29Dec/src/Lec18/NextGreaterElement.java
+++ Crux29Dec/src/Lec18/NextGreaterElement.java
@@ -7,7 +7,10 @@ public class NextGreaterElement {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		int[] arr = { 50, 20, 10, 40, 30, 60, 15, 12 };
-		printNextGreater(arr);
+		int[] res = getNextGreaterInOrder(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i] + "," + res[i]);
+		}
 	}
 
 	public static void printNextGreater(int[] arr) throws Exception {
@@ -15,28 +18,29 @@ public class NextGreaterElement {
 		Stack s = new Stack(arr.length);
 		for (int i = 0; i < arr.length; i++) {
 			while (!s.isEmpty() && s.peek() < arr[i]) {
-				System.out.println(s.pop() + " -> " + arr[i]);
+				System.out.println(s.pop() + "," + arr[i]);
 			}
 			s.push(arr[i]);
 		}
 		while (!s.isEmpty()) {
-			System.out.println(s.pop() + " -> " + -1);
+			System.out.println(s.pop() + "," + -1);
 		}
 
 	}
 	
-	public static void printNextGreaterInOrder(int[] arr) throws Exception {
+	public static int[] getNextGreaterInOrder(int[] arr) throws Exception {
 		Stack s = new Stack(arr.length);
 		int[] arr1 = new int[arr.length];
 		for (int i = 0; i < arr.length; i++) {
-			while (!s.isEmpty() && s.peek() < arr[i]) {
-				System.out.println(s.pop() + " -> " + arr[i]);
+			while (!s.isEmpty() && arr[s.peek()] < arr[i]) {
+				arr1[s.pop()] = arr[i];
 			}
-			s.push(arr[i]);
+			s.push(i);
 		}
 		while (!s.isEmpty()) {
-			System.out.println(s.pop() + " -> " + -1);
+			arr1[s.pop()] = -1;
 		}
+		return arr1;
 	}
 
 }
