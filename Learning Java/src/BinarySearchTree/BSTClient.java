8569e32facb2b897c9baa12af7782a25f1ca054d
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-03-27 05:50:31 +0530

    updated find, getmax, add & remove item

--- Learning Java/src/BinarySearchTree/BSTClient.java
+++ Learning Java/src/BinarySearchTree/BSTClient.java
@@ -6,8 +6,16 @@ public class BSTClient {
 		// TODO Auto-generated method stub
 		int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
 		BinarySearchTree  bst = new BinarySearchTree(arr);
+//		System.out.println(bst.find(50));
+//		System.out.println(bst.getMaximum());
+//		bst.add(55);
+//		bst.add(27);
 		bst.display();
-		
+		System.out.println();
+		bst.remove(60);
+		bst.display();
 	}
+	
+	
 
 }
--- Learning Java/src/BinarySearchTree/BinarySearchTree.java
+++ Learning Java/src/BinarySearchTree/BinarySearchTree.java
@@ -4,8 +4,8 @@ public class BinarySearchTree {
 
 	private class Node{
 		int data;
-		Node left;
-		Node right;
+		Node left = null;
+		Node right = null;
 	}
 	
 	private Node root;
@@ -45,6 +45,107 @@ public class BinarySearchTree {
 		System.out.println(str);
 		display(node.left);
 		display(node.right);
+	}
+	
+	public boolean find(int item) {
+		return find(this.root, item);
+	}
+	
+	private boolean find(Node node, int item) {
+		if(node == null)
+			return false;
+		if(node.data == item)
+			return true;
+		if(item < node.data)
+			return find(node.left, item);
+		else
+			return find(node.right, item);
+	}
+	
+	public int getMaximum() {
+		return getMaximum(this.root);
+	}
+	
+	private int getMaximum(Node node) {
+		
+		if(node.right == null)
+			return node.data;
+		return getMaximum(node.right);
+		
+//		Node start = this.root;
+//		while(start.right != null)
+//			start = start.right;
+//		return start.data;
+	}
+	
+	public void add(int item) {
+		add(this.root, item);
+	}
+	
+	private void add(Node node, int item) {
+
+		if(item <= node.data) {
+			if(node.left == null){
+				Node nn = new Node();
+				nn.data = item;
+				node.left = nn;
+				return;
+			}
+			add(node.left, item);
+		}
+		else {
+			if(node.right == null){
+				Node nn = new Node();
+				nn.data = item;
+				node.right = nn;
+				return;
+			}
+			add(node.right, item);
+		}
+	}
+	
+	public void remove(int item) {
+		this.remove(this.root, null, false, item);
+	}
+	
+	private void remove(Node node, Node parent, boolean isleft, int item) {
+		if(node == null)
+			return;
+		if(item < node.data) {
+			remove(node.left, node, true, item);
+		}else if(item > node.data){
+			remove(node.right, node, false, item);
+		}else {
+			
+			if(node.left == null && node.right == null) {
+				if(isleft)
+					parent.left = null;
+				else
+					parent.right = null;
+			}
+			
+			else if(node.left == null && node.right != null) {
+				if(isleft)
+					parent.left = node.right;
+				else
+					parent.right = node.right;
+			}
+			
+			else if(node.left != null && node.right == null) {
+				if(isleft)
+					parent.left = node.left;
+				else
+					parent.right = node.left ;
+			}
+			
+			else {
+				// right != null & left != null
+				int max = getMaximum(node.left);
+				node.data = max;
+				remove(node.left, node, true, max);
+			}
+
+		}
 	}
 	
 }
