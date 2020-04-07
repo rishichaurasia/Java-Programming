111860d155204388f6f4dcd11b3f9a27998fecda
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-04-07 05:50:12 +0530

    Incomplete

---
+++ Learning Java/src/BitManipulation/MagicNumbers.java
@@ -0,0 +1,23 @@
+package BitManipulation;
+
+public class MagicNumbers {
+
+	public static void main(String[] args) {
+		//Magic nos - Numbers which are power of 5 or sum of powers of 5.
+		// Generate Magic nos. i.e. 5, 25, 30, etc
+/*
+ *    5^3  5^2  5^1
+ *    -------------
+ *     0    0    1	-> 1 = 5
+ *     0	1	 0  -> 2 = 25
+ *     0	1	 1  -> 3 = 30
+ *     1	0 	 0  -> 4 = 125
+ *     1	0	 1  -> 5 = 130
+ *     1	1 	 0  -> 6 = 150
+ *     1	1 	 1  -> 7 = 155
+ *     
+ */
+
+	}
+
+}
