bb3747428be6abc6193eed50cbba074702b7671e
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-04-09 18:34:54 +0530

    Updated

--- Learning Java/src/GenericTrees/GenericTreeClient.java
+++ Learning Java/src/GenericTrees/GenericTreeClient.java
@@ -6,11 +6,21 @@ public class GenericTreeClient {
 		// input : 10 3 20 2 50 0 60 1 80 0 30 0 40 1 70 0
 		GenericTree gt = new GenericTree();
 		gt.display();
-		System.out.println(gt.size());
-		System.out.println(gt.getMax());
-		System.out.println(gt.height());
-		System.out.println(gt.find(60));
-		System.out.println(gt.find(90));
+//		System.out.println(gt.size());
+//		System.out.println(gt.getMax());
+//		System.out.println(gt.height());
+//		System.out.println(gt.find(60));
+//		System.out.println(gt.find(90));
+//		gt.mirror();
+//		gt.display();
+//		gt.printAtLevel(2);
+//		gt.linearize();
+//		gt.display();
+		gt.preorder();
+		gt.postorder();
+		gt.levelorder();
+		gt.levelorderByLine1();
+		gt.levelorderByLine2();
 	}
 
 }
