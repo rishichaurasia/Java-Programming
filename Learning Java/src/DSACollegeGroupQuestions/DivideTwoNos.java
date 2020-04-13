53c9c3f769704c6b08b7aedccf101eebdb9fb2a4
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-04-13 20:50:45 +0530

    Divide using Bit Manipulation

--- Learning Java/src/DSACollegeGroupQuestions/DivideTwoNos.java
+++ Learning Java/src/DSACollegeGroupQuestions/DivideTwoNos.java
@@ -11,16 +11,23 @@ public class DivideTwoNos {
 		int m = scn.nextInt();
 		System.out.println("Enter Divisor");
 		int n = scn.nextInt();
-		int[] res = divide(m, n);
-		System.out.println("Quotient = " + res[0] + "\nRemainder = " + res[1]);
-		
+//		int[] res = divide(m, n);
+//		System.out.println("Quotient = " + res[0] + "\nRemainder = " + res[1]);
+		long s1 = System.currentTimeMillis();
+		System.out.println(divideBW(m, n));
+		long e1 = System.currentTimeMillis();
+		System.out.println(e1-s1);
+		long s2 = System.currentTimeMillis();
+		System.out.println(divideBW1(m, n));
+		long e2 = System.currentTimeMillis();
+		System.out.println(e2-s2);
 	}
 	
 	public static int[] divide(int dividend, int divisor) throws Exception {
 		if(divisor == 0)
 			throw new Exception("ZeroDivisionError");
 		int[] quotRem = {0, 0};
-		if(divisor == 0)
+		if(dividend == 0)
 			return quotRem;
 		int sign = 1;
 		if((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
@@ -34,6 +41,44 @@ public class DivideTwoNos {
 		quotRem[0] *= sign;
 		quotRem[1] = dividend;
 		return quotRem;
+	}
+	
+	public static int divideBW(int dividend, int divisor) throws Exception {
+		if(divisor == 0)
+			throw new Exception("ZeroDivisionError");
+		int sign = (dividend < 0 ^ divisor < 0) ? -1 : 1;
+		int dvd = Math.abs(dividend);
+		int dvs = Math.abs(divisor);
+		int q = 0;
+		for(int i = 31 ; i>=0; i--) {
+			int temp = dvs << i;
+			if (dvd >= temp && temp >= dvs)  
+		    {    
+		        dvd -= temp;
+				q += (1 << i);  
+		    } 
+		}
+		return sign * q;
+	}
+	
+	public static int divideBW1(int dividend, int divisor) throws Exception {
+		if(divisor == 0)
+			throw new Exception("ZeroDivisionError");
+		int sign = (dividend < 0 ^ divisor < 0) ? -1 : 1;
+		int dvd = Math.abs(dividend);
+		int dvs = Math.abs(divisor);
+		int q = 0;
+		while(dvd >= dvs) {
+			int r = 0;
+			int tdvs = dvs;
+			while(dvd >= (tdvs << 1)) {
+				tdvs = tdvs << 1;
+				r++;
+			}
+			q += (1 << r);
+			dvd -= tdvs;
+		}
+		return sign * q;
 	}
 
 }
