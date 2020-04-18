1c2b45853c837afdb8634aa25a7f5cfc7db0f73b
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-04-18 17:52:45 +0530

    Added new methods to BT

--- Learning Java/src/BinaryTree/BinaryTree.java
+++ Learning Java/src/BinaryTree/BinaryTree.java
@@ -15,16 +15,16 @@ public class BinaryTree {
 			this.RChild = rChild;
 		}
 	}
-
+	
+	Scanner scn = new Scanner(System.in);
 	private Node root = null;
 	private int size = 0;
 
 	BinaryTree() {
-		Scanner scn = new Scanner(System.in);
-		this.root = takeInput(scn, null, false);
+		this.root = takeInput(null, false);
 	}
 
-	public Node takeInput(Scanner s, Node parent, Boolean isLeft) {
+	public Node takeInput(Node parent, Boolean isLeft) {
 		if (parent == null)
 			System.out.println("Enter the data for the root node");
 		else if (isLeft)
@@ -33,15 +33,15 @@ public class BinaryTree {
 			System.out.println("Enter the data for the right child of " + parent.data);
 		}
 
-		int nodeData = s.nextInt();
+		int nodeData = scn.nextInt();
 		Node node = new Node(nodeData, null, null);
 		this.size++;
 		System.out.println("Do you have left child of " + node.data + "?");
-		if (s.nextBoolean())
-			node.LChild = takeInput(s, node, true);
+		if (scn.nextBoolean())
+			node.LChild = takeInput(node, true);
 		System.out.println("Do you have right child of " + node.data + "?");
-		if (s.nextBoolean())
-			node.RChild = takeInput(s, node, false);
+		if (scn.nextBoolean())
+			node.RChild = takeInput(node, false);
 		return node;
 	}
 
@@ -54,14 +54,14 @@ public class BinaryTree {
 			return;
 		String str = "";
 		if (node.LChild == null)
-			str += "END=>";
+			str += "END->";
 		else
-			str += node.LChild.data + "=>";
+			str += node.LChild.data + "->";
 		str += node.data;
 		if (node.RChild == null)
-			str += "<=END";
+			str += "<-END";
 		else
-			str += "<=" + node.RChild.data;
+			str += "<-" + node.RChild.data;
 		System.out.println(str);
 		display(node.LChild);
 		display(node.RChild);
@@ -131,6 +131,136 @@ public class BinaryTree {
 				queue.add(rv.RChild);
 		}
 		System.out.println("END");
+	}
+	
+	public int size() {
+		return this.size(this.root);
+	}
+	
+	private int size(Node node) {
+		if(node == null)
+			return 0;
+		return 1 + size(node.LChild) + size(node.RChild); 
+	}
+	
+	public int max() {
+		return this.max(this.root);
+	}
+
+	private int max(Node node) {
+		if(node == null)
+			return Integer.MIN_VALUE;
+		int max = node.data;
+		int left = max(node.LChild);
+		int right = max(node.RChild);
+		if(left > max)
+			max = left;
+		if(right > max)
+			max = right;
+		return max;
+	}
+	
+	public boolean find(int item) {
+		return this.find(this.root, item);
+	}
+
+	private boolean find(Node node, int item) {
+		if(node == null)
+			return false;
+		if(node.data == item)
+			return true;
+		return find(node.LChild, item) || find(node.RChild, item);
+	}
+	
+	public int height1() {
+		return this.height1(this.root);
+	}
+
+	private int height1(Node node) {
+		if(node == null)
+			return -1;
+		int max = Math.max(height1(node.LChild), height1(node.LChild));
+		return max + 1;
+	}
+	
+	public int diameter() {
+		return diameter(this.root);
+	}
+	
+	private int diameter(Node node) {
+		if(node == null)
+			return 0;
+		int lh = height(node.LChild);
+		int rh = height(node.RChild);
+		int curr_dia = lh + rh + 2;
+		return Math.max(curr_dia, Math.max(diameter(node.LChild), diameter(node.RChild)));
+	}
+	
+	public int diameter2() {
+		return diameter2(this.root).dia;
+	}
+	
+	private class Diapair{
+		int dia = 0;
+		int ht = -1;
+	}
+	
+	private Diapair diameter2(Node node) {
+		if(node == null)
+			return new Diapair();
+		Diapair ldp = diameter2(node.LChild);
+		Diapair rdp = diameter2(node.RChild);
+		Diapair sdp = new Diapair();
+		sdp.dia = Math.max(ldp.ht + rdp.ht + 2, Math.max(ldp.dia, rdp.dia));
+		sdp.ht = Math.max(ldp.ht, rdp.ht) + 1;
+		return sdp;
+	}
+	
+	public boolean isBalanced() {
+		return isBalanced(this.root).isbal;
+	}
+	
+	private class Balpair{
+		int ht = -1;
+		boolean isbal = true;
+	}
+	
+	private Balpair isBalanced(Node node) {
+		if(node == null)
+			return new Balpair();
+		Balpair lbp = isBalanced(node.LChild);
+		Balpair rbp = isBalanced(node.RChild);
+		Balpair sbp = new Balpair();
+		int bf = lbp.ht - rbp.ht;
+		boolean isBal = false;
+		if(bf == 1 || bf == 0 || bf == -1)
+			isBal = true;
+		sbp.isbal = lbp.isbal && rbp.isbal && isBal;
+		sbp.ht = Math.max(lbp.ht, rbp.ht) + 1;
+		return sbp;
+		
+	}
+	
+	public boolean flipEqivalent(BinaryTree other) {
+		return flipEqivalent(this.root,other.root);
+	}
+
+	private boolean flipEqivalent(Node node1, Node node2) {
+		if(node1 == null && node2 == null)
+			return true;
+		if(node1 == null && node2 != null)
+			return false;
+		if(node1 != null && node2 == null)
+			return false;
+		if(node1.data != node2.data)
+			return false;
+		boolean ll = flipEqivalent(node1.LChild, node2.LChild);
+		boolean rr = flipEqivalent(node1.RChild, node2.RChild);
+		if(ll && rr)
+			return true;
+		boolean lr = flipEqivalent(node1.LChild, node2.RChild);
+		boolean rl = flipEqivalent(node1.RChild, node2.LChild);
+		return (lr && rl);
 	}
 	
 }
--- Learning Java/src/BinaryTree/BinaryTreeClient.java
+++ Learning Java/src/BinaryTree/BinaryTreeClient.java
@@ -5,11 +5,18 @@ public class BinaryTreeClient {
 	public static void main(String[] args) {
 		// Input : 50 true 25 true 38 false false true 48 true 18 false false false true 45 true 85 false false true 60 false false
 		BinaryTree bt = new BinaryTree();
-//		bt.display();
-		bt.preorderTraversal();
-		bt.postorderTraversal();
-		bt.inorderTraversal();
-		bt.levelOrder();
+		bt.display();
+//		System.out.println(bt.size());
+//		System.out.println(bt.max());
+//		System.out.println(bt.find(60));
+//		System.out.println(bt.height1());
+		// Input: 10 true 20 true 40 true 70 true 90 true 110 false false false false false true 50 false true 80 false true 100 false true 120 false false true 30 false true 60 false false 
+//		System.out.println(bt.diameter());
+		System.out.println(bt.isBalanced());
+//		bt.preorderTraversal();
+//		bt.postorderTraversal();
+//		bt.inorderTraversal();
+//		bt.levelOrder();
 	}
 
 }
