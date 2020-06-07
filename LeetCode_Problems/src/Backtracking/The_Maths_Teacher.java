728abf8a4a03a204ab69fd8822bfc61bb41a640a
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-06-08 04:44:35 +0530

    LeetCode

---
+++ LeetCode_Problems/src/Backtracking/The_Maths_Teacher.java
@@ -0,0 +1,28 @@
+package Backtracking;
+
+import java.util.Scanner;
+
+public class The_Maths_Teacher {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		while(t-- > 0) {
+			int n = scn.nextInt();
+		}
+
+	}
+	
+//	public static boolean ifPossible(int n) {
+//		int i = 1;
+//		if(n==1)
+//			return true;
+//		while((i<<1) <= n) {
+//			i = i<<1;
+//		}
+//		if(i == n)
+//			return true;
+//		
+//	}
+
+}
---
+++ LeetCode_Problems/src/LeetCode_June_Challenge/Queue_Reconstruction_by_Height.java
@@ -0,0 +1,38 @@
+package LeetCode_June_Challenge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Queue_Reconstruction_by_Height {
+
+	public static void main(String[] args) {
+		int[][] people = {{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}};
+		reconstructQueue(people);
+
+	}
+	
+	public static int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+        	@Override
+        	public int compare(int[] a, int[] b) {
+        		if(a[0] != b[0])
+        			return b[0] - a[0];
+        		else
+        			return a[1] - b[1];
+        	}
+		});
+        
+        ArrayList<int[]> queue = new ArrayList<>();
+        for(int[] person: people) {
+        	queue.add(person[1], person);
+        }
+        
+        int[][] res = new int[people.length][];
+        for (int i = 0; i < res.length; i++) {
+			res[i] = queue.get(i);
+		}
+        return res;
+    }
+
+}
