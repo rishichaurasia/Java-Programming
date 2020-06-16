1a161771d5c09fbaca0813bb986e04525937baae
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-06-17 03:07:19 +0530

    Create Validate_IP_Address.java

---
+++ LeetCode_Problems/src/LeetCode_June_Challenge/Week_3/Validate_IP_Address.java
@@ -0,0 +1,52 @@
+package LeetCode_June_Challenge.Week_3;
+
+public class Validate_IP_Address {
+
+	public static void main(String[] args) {
+		String IP = "2001:0db8:85a3:0:0:8A2E:0370:";
+		System.out.println(validIPAddress(IP));
+
+	}
+
+	public static String validIPAddress(String IP) {
+		if (IP.chars().filter(ch -> ch == '.').count() == 3) {
+			return validateIPv4(IP);
+		} else if (IP.chars().filter(ch -> ch == ':').count() == 7) {
+			return validateIPv6(IP);
+		} else
+			return "Neither";
+	}
+
+	public static String validateIPv4(String IP) {
+		String[] nums = IP.split("\\.", -1);
+		for (String x : nums) {
+			if (x.length() == 0 || x.length() > 3)
+				return "Neither";
+			if (x.charAt(0) == '0' && x.length() != 1)
+				return "Neither";
+			for (char ch : x.toCharArray()) {
+				if (!Character.isDigit(ch))
+					return "Neither";
+			}
+			if (Integer.parseInt(x) > 255)
+				return "Neither";
+		}
+		return "IPv4";
+	}
+
+	public static String validateIPv6(String IP) {
+		String[] nums = IP.split("\\:", -1);
+		String hexdigits = "0123456789abcdefABCDEF";
+		for (String x : nums) {
+			System.out.println(x);
+			if (x.length() == 0 || x.length() > 4)
+				return "Neither";
+			for (Character ch : x.toCharArray()) {
+				if (hexdigits.indexOf(ch) == -1)
+					return "Neither";
+			}
+		}
+		return "IPv6";
+	}
+
+}
