8708e3b24b582a6ffd2a4bb4383974b9c8d0e855
rishichaurasia <rishi.r.chaurasia@gmail.com>
2020-06-30 08:45:50 +0530

    New Problems

---
+++ Learning Java/src/Dynamic_Programming/Matrix_Chain_Multiplication.java
@@ -0,0 +1,96 @@
+package Dynamic_Programming;
+
+/*
+ * To find the minimum number of multiplication operations needed to multiply the matrices of 
+ * given chain order.
+ */
+public class Matrix_Chain_Multiplication {
+
+	public static void main(String[] args) {
+		int[] matrix = {4,2,3,5,1};
+//		int[] matrix = new int[1000];
+//		for (int i = 0; i < 1000; i++)
+//			matrix[i] = i + 1;
+		int n = matrix.length;
+//		System.out.println(MCMRecur(matrix, 0, n-1));
+		System.out.println(MCMTD(matrix, 0, n - 1, new int[n][n]));
+		System.out.println(MCMBU(matrix));
+
+	}
+
+	private static int MCMRecur(int[] matrix, int si, int ei) {
+		if (si + 1 == ei)
+			return 0;
+		int minOperations = Integer.MAX_VALUE;
+
+		// k : splits
+		for (int k = si + 1; k <= ei - 1; k++) {
+			int part1 = MCMRecur(matrix, si, k); // matrix[si] * matrix[k]
+			int part2 = MCMRecur(matrix, k, ei); // matrix[k] * matrix[ei]
+
+			int whole = matrix[si] * matrix[k] * matrix[ei]; // matrix[si] * matrix[ei]
+
+			int totalOperations = part1 + part2 + whole;
+			minOperations = Math.min(minOperations, totalOperations);
+		}
+		return minOperations;
+	}
+
+	private static int MCMTD(int[] matrix, int si, int ei, int[][] strg) {
+		if (si + 1 == ei)
+			return 0;
+		if (strg[si][ei] != 0)
+			return strg[si][ei];
+
+		int minOperations = Integer.MAX_VALUE;
+
+		// k : splits
+		for (int k = si + 1; k <= ei - 1; k++) {
+			int part1 = MCMTD(matrix, si, k, strg); // matrix[si] * matrix[k]
+			int part2 = MCMTD(matrix, k, ei, strg); // matrix[k] * matrix[ei]
+
+			int whole = matrix[si] * matrix[k] * matrix[ei]; // matrix[si] * matrix[ei]
+
+			int totalOperations = part1 + part2 + whole;
+			minOperations = Math.min(minOperations, totalOperations);
+		}
+		strg[si][ei] = minOperations;
+		return minOperations;
+	}
+
+	private static int MCMBU(int[] matrix) {
+		int n = matrix.length;
+		int[][] strg = new int[n][n];
+		for (int slide = 1; slide <= n; slide++) {
+			for (int si = 0; si <= n - slide - 1; ++si) {
+				int ei = si + slide;
+
+				if (si + 1 == ei)
+					continue;
+
+				int minOperations = Integer.MAX_VALUE;
+
+				// k : splits
+				for (int k = si + 1; k <= ei - 1; k++) {
+					int part1 = strg[si][k]; // matrix[si] * matrix[k]
+					int part2 = strg[k][ei]; // matrix[k] * matrix[ei]
+
+					int whole = matrix[si] * matrix[k] * matrix[ei]; // matrix[si] * matrix[ei]
+
+					int totalOperations = part1 + part2 + whole;
+
+					minOperations = Math.min(minOperations, totalOperations);
+				}
+				strg[si][ei] = minOperations;
+
+			}
+		}
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<n; j++)
+				System.out.print(strg[i][j] + "\t ");
+			System.out.println();
+		}
+		return strg[0][n - 1];
+	}
+
+}
--- LeetCode_Problems/src/LeetCode_June_Challenge/Week_5/Find_the_Duplicate_Number.java
+++ LeetCode_Problems/src/LeetCode_June_Challenge/Week_5/Find_the_Duplicate_Number.java
@@ -1,6 +1,8 @@
 package LeetCode_June_Challenge.Week_5;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Find_the_Duplicate_Number {
 
@@ -8,17 +10,62 @@ public class Find_the_Duplicate_Number {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	// O(nlgn) Time | O(1) Space
 	public static int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1]) {
-                return nums[i];
-            }
-        }
+		Arrays.sort(nums);
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] == nums[i - 1]) {
+				return nums[i];
+			}
+		}
 
-        return -1;
-    }
+		return -1;
+	}
+
+	// O(n) Time | O(1) space
+	public int findDuplicate1(int[] nums) {
+		Set<Integer> seen = new HashSet<Integer>();
+		for (int num : nums) {
+			if (seen.contains(num)) {
+				return num;
+			}
+			seen.add(num);
+		}
+
+		return -1;
+	}
+
+	// O(n) Time | O(1) space | Mutable Array
+	public static int findDuplicate2(int[] nums) {
+
+		for (int i = 0; i < nums.length; ++i) {
+			if (nums[Math.abs(nums[i])] >= 0)
+				nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i])];
+			else
+				return Math.abs(nums[i]);
+		}
+		return 0;
+	}
+
+	// O(n) Time | O(1) space | Immutable Array
+	public static int findDuplicate3(int[] nums) {
+		// Find the intersection point of the two runners.
+		int tortoise = nums[0];
+		int hare = nums[0];
+		do {
+			tortoise = nums[tortoise];
+			hare = nums[nums[hare]];
+		} while (tortoise != hare);
+
+		// Find the "entrance" to the cycle.
+		tortoise = nums[0];
+		while (tortoise != hare) {
+			tortoise = nums[tortoise];
+			hare = nums[hare];
+		}
+
+		return hare;
+	}
 
 }
